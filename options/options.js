63a64f3dc1111176369f678170c9d50e81d01c30
Crowdbotics <engineering@crowdbotics.com>
2023-08-15 07:27:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sidebar-project-de-109356",
+  url: "https://sidebar-project-de-109356.botics.co",
+  api: "https://sidebar-project-de-109356.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
